ea9b97a1fac7a145605d30b85bd32f9fe6d1ed85
FranckPharel <franckfoussom93@gmail.com>
2020-12-23 18:17:09 +0100

    creation des controller puis debut de gestion des employées

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,5 +76,7 @@
             @yield('content')
         </main>
     </div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 */
+
+Route::group(['prefix' => '', 'namespace' => 'ModulesControllers', 'as' => 'api.', 'middleware' => ['api']], function () {
+    Route::resource('employee', 'EmployeeController', ['except' => ['create', 'edit']]);
+});
